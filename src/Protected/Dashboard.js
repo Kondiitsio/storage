031d25558ec98c12bf9aeffc53e84c6516ce9e48
kondiitsio <anssi.jankala@gmail.com>
2024-05-16 14:50:19 +0300

    SCM_DO_BUILD_DURING_DEPLOYMENT

--- src/Protected/Dashboard.js
+++ src/Protected/Dashboard.js
@@ -5,7 +5,7 @@ function Dashboard() {
   return (
     <>
       <Layout>
-        <h1>Welcome!!</h1>
+        <h1>Welcome!</h1>
         <h1>This application is under development, you can test how Stores & Items works.</h1>
         <p>Data is not important, so no worries if you delete something.</p>
       </Layout>
